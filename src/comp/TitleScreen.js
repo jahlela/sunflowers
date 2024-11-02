36e45ed48fb709a8a7a965eb7f9702e3e65074c9
jahlela <jahlela5@gmail.com>
2024-11-02 15:51:19 +0100

    Add logo and sytling to landing page

--- src/comp/TitleScreen.js
+++ src/comp/TitleScreen.js
@@ -6,6 +6,12 @@ const TitleScreen = ({gameStart}) => {
     //gameStart is passed up to App.js so we can use onClick to change the state into having started the game
     return (
         <div className="titleScreen">
+            <img src={require("../img/dreampath_logo.png")} alt="Dream PATH Logo" className="logo" />
+            {/* <h2 className='mainTitle'>Welcome to Dream PATH!</h2> */}
+            <h3 className='mainText'>Discover your future 🔮</h3>
+            <div className='mainText'>Hey there, future leaders! 🌟 Welcome to Dream Path - the ultimate hub for teens in Switzerland to explore apprenticeship and internship opportunities.
+Find engaging assessments and real-life career scenarios. Start your journey towards an exciting future with us today!
+</div>
             <div className="titleScreenChoices">
                 <Choice handleClick={gameStart} text="Let's go! 😀" />
             </div>
